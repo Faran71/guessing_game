97baaed18fe8d1c456d1a549af6532cc1abca5c6
Faran71 <muhammadfaran01.mfs@gmail.com>
2023-07-06 15:25:34 +0100

    guess game is made

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,2 +1,14 @@
 public class GuessingGame {
+    public static void main(String[] args) {
+        int secretNumber = 15;
+        int guess = 17;
+        if(guess == secretNumber){
+            System.out.println("Guess is correct");
+        } else if(guess > secretNumber){
+            System.out.println("Guess is too high");
+        } else {
+            System.out.println("Guess is too low");
+        }
+
+    }
 }
